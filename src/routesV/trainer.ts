57a8ce1050409135908e2a7d10ded549330f2a44
nawatend <32384185+nawatend@users.noreply.github.com>
2020-04-19 15:20:04 +0200

    packages install 4

--- src/routesV/trainer.ts
+++ src/routesV/trainer.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { TrainerController } from "../controller/trainerController";
+import { TrainerController } from "../controller/TrainerController";
 
 const trainerRouter = express.Router()
 
